d7c8a163f469571d45d8da5370d28451dae65423
Gropers <1373314437@qq.com>
2026-03-30 20:47:57 +0800

    Site updated: 2026-03-30 20:47:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/30/DC-1/","2025/12/09/ova靶机/","2025/12/09/Gropers-first-blog/","2026/03/30/reset/","2025/12/09/hello-world/"];function toRandomPost(){
+var posts=["2026/03/30/DC-1/","2025/12/09/Gropers-first-blog/","2026/03/30/reset/","2025/12/09/hello-world/","2025/12/09/ova靶机/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
