9b40db6f3fc3f0a06e2e472f4666aaf5460fdfe8
Gropers <1373314437@qq.com>
2026-04-01 20:54:00 +0800

    Site updated: 2026-04-01 20:54:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/12/DC-1/","2025/12/13/bp攻击/","2025/12/09/Gropers-first-blog/","2025/12/09/hello-world/","2025/03/20/reset/","2025/12/09/ova靶机/"];function toRandomPost(){
+var posts=["2025/12/12/DC-1/","2025/12/09/Gropers-first-blog/","2025/12/13/bp攻击/","2025/12/09/hello-world/","2025/03/20/reset/","2025/12/09/ova靶机/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
