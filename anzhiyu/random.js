14736b0e267667a2dcd5840354978a3bcac73788
Gropers <1373314437@qq.com>
2026-04-03 14:07:05 +0800

    Site updated: 2026-04-03 14:07:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/12/DC-1/","2025/12/09/Gropers-first-blog/","2025/12/13/bp攻击/","2025/12/09/ova靶机/","2026/03/29/群友靶机--JNDI/","2026/03/20/群友靶机--reset/","2026/03/25/群友靶机--Re/","2025/12/09/hello-world/","2026/03/26/群友靶机-lanSSudoyy/","2026/03/30/群友靶机-HackMe/"];function toRandomPost(){
+var posts=["2025/12/12/DC-1/","2025/12/13/bp攻击/","2025/12/09/ova靶机/","2026/03/29/群友靶机--JNDI/","2026/03/20/群友靶机--reset/","2026/03/30/群友靶机-HackMe/","2026/03/25/群友靶机--Re/","2025/12/09/hello-world/","2026/03/26/群友靶机-lanSSudoyy/","2025/12/09/Gropers-first-blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
